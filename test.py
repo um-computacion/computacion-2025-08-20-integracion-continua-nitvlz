e0d4b6e79a286688c1a8b08e13e8e195ecf02199
nitvlz <tinchovlz16@gmail.com>
2025-08-27 08:14:51 -0300

    implementar la funcion resta

--- test.py
+++ test.py
@@ -7,5 +7,8 @@ class TestPrueba(unittest.TestCase):
     def test_suma(self):
         self.assertEqual(suma(2, 3), 5)
 
+    def test_resta(self):
+        self.assertEqual(suma(5, -3), 2)
+ 
 if __name__ == "__main__":
     unittest.main()
